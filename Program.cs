e803d73c9e23d8052090a7a02b58b81fe74f5c26
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-15 22:48:56 +0100

    Exercice1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercice_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 12;
+            int secondNumber = 34;
+
+            Console.WriteLine("Le premier nombre est " + firstNumber);
+            Console.WriteLine("Le second nombre est " + secondNumber);
+
+            int result = firstNumber + secondNumber;
+
+            Console.WriteLine("Le résultat est " + result);
+        }
+    }
+}
